063d042eb5db0d2b34a99645deca91c1b7236a03
SimonEvs <simonevseev07@mail.ru>
2022-11-24 20:17:16 +0300

    new model

---
+++ VendingMachine1/src/model/Bottle.java
@@ -0,0 +1,34 @@
+package VendingMachine1.src.model;
+
+public class Bottle extends Product{
+ 
+    private double volume;
+
+    public Bottle(String name, double cost, double volume) {
+        super(name, cost);
+        this.volume=volume;
+        
+    }
+
+    public double getVolume() {
+        return volume;
+    }
+
+    public void setVolume(double volume) {
+        this.volume = volume;
+    }
+
+    // public double getPrice(){
+    //     return getCost()*volume;
+    // }
+    
+    public double getCost(){
+        return super.getCost()*getVolume();
+    }
+
+    @Override
+    public String toString() {
+        
+        return super.toString()+" "+ volume+"л.";
+    }
+}
